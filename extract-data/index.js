1d61a5655c41b3a953e6ba0dfa7047b283e86a9a
Andreas Bäckevik <a.backevik@gmail.com>
2020-04-09 17:11:55 +0200

    improve path input validation

--- extract-data/index.js
+++ extract-data/index.js
@@ -28,11 +28,27 @@ const parse = (file) => {
   fs.writeFileSync('./src/static/app-data.json', JSON.stringify(reduceAstNode([], initialAst)[0]));
 };
 
-const getRootComponent = (name) => {
-  const files = fs.readdirSync('.');
-  const rootComponent = files.find((fileName) => fileName === name || fileName === `${name}.js` || fileName === `${name}.jsx`);
-  if (rootComponent) {
-    parse(rootComponent);
+const getRootComponent = (path) => {
+  const pathTree = path.split('/');
+  let files = [];
+  let fileName = '';
+  // root component is at root level
+  if(pathTree.length === 1) {
+    files = fs.readdirSync('.');
+    fileName = pathTree[0];
+  // else path contains folders
+  } else {
+    const pathToRootComponent = pathTree.slice(0, pathTree.length - 1).join('/');
+    files = fs.readdirSync(`./${pathToRootComponent}`);
+    fileName = pathTree[pathTree.length - 1];
+  }
+  // check if input file type is valid
+  const fileIsValid = files.some((name) => 
+    fileName.includes('js') || 
+    fileName.includes('jsx') && 
+    name === fileName);
+  if (fileIsValid) {
+    parse(path);
   } else {
     throw new Error('ERROR: Root component not found. Are you sure you are in the right folder?');
   }
