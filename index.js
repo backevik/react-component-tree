f6a30e772d355aa191a78e72a4e526bcde028544
Andreas Bäckevik <a.backevik@gmail.com>
2020-04-09 17:25:51 +0200

    add -o option to set webpack build path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const path = require('path');
 
 const getArg = () => {
   const args = process.argv.slice(2);
@@ -8,5 +9,23 @@ const getArg = () => {
   throw new Error('Invalid argument provided');
 };
 
+const getOutputPath = () => {
+  const args = process.argv.slice(2);
+  const outputIndex = args.findIndex((arg) => arg === '-o');
+  // user provided output
+  if(outputIndex > -1) {
+    const folder = args[outputIndex + 1];
+    // did not provide any value
+    if(!folder) {
+      throw new Error('No argument provided to output.');
+    }
+    return path.resolve(process.cwd(), folder);
+  }
+  // default output folder
+
+  // TODO: IF FOLDER DOES NOT EXIST CREATE IT!
+  return path.resolve(process.cwd(), 'dist');
+}
+
 execSync(`yarn parse ${getArg()}`, { stdio: [0, 1, 2] });
-execSync('yarn build', { stdio: [0, 1, 2] });
+execSync(`yarn build --output-path ${getOutputPath()}`, { stdio: [0, 1, 2] });
